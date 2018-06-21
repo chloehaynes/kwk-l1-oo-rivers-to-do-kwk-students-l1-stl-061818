6d08b463dc191525ed9ebb8ac9958bc1d53765c5
chloehaynes <19chaynes@nerinxhs.org>
2018-06-21 21:09:23 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -23,7 +23,7 @@ river1 = River.new("The Nile", 4258, "Egypt, Sudan",  99940)
 river2 = River.new("The Mississippi", 2320, "United States of America",  593000)
 river3 = River.new("The Amazon", 4345, "Brazil, Peru, Columbia", 7381000)
 river4 = River.new("The Seine", 482, "France", 17660)
-river5 = RIver.new("The Yangtze", 3915, "China", 1065000)
+river5 = River.new("The Yangtze", 3915, "China", 1065000)
 river6 = River.new("The Euphrates", 1740, "Syria, Turkey, Iraq", 12570)
 
 river3.flood
