2306ce5f8c68ce4de00d538b1fdfd288bb9db7a5
chloehaynes <19chaynes@nerinxhs.org>
2018-06-21 21:01:59 +0000

    automatically backed up by learn

--- river.rb
+++ river.rb
@@ -19,7 +19,8 @@ class River
   
 end 
 
-river1 = River.new("The Nile", 4258, "Egypt, Sudan", )
+river1 = River.new("The Nile", 4258, "Egypt, Sudan",  99940)
+river2 = River.new("The Mississippi, ")
     
   
 
